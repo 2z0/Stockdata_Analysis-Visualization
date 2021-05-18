b19513eca988c1846f7c6bf32aaf1a950ae5328b
2z0 <71939195+2z0@users.noreply.github.com>
2021-05-18 22:45:59 +0900

    Update views.py

--- views.py
+++ views.py
@@ -6,33 +6,6 @@ from .models import CompanyInfo
 
 from django.views.generic.list import ListView
 
-"""
-class ListView(ListView):
-    model = CompanyInfo
-
-def BookListView(request):
-    #
-    # books = Book.objects.all()
-    def __init__(self):
-        try:
-
-            self.conn = pymysql.connect(host='fadodb.cdhgfueefwts.ap-northeast-2.rds.amazonaws.com', user='root',
-                                        password='Cseadmin1!', db='INVESTAR', charset='utf8')
-
-            cursor = self.conn.cursor()
-
-            strSql = "SELECT code, company FROM company_info"
-            result = cursor.execute(strSql)
-            datas = cursor.fetchall()
-            self.conn.close()
-
-        except:
-            self.conn.rollback()
-            print("Failed selecting in BookListView")
-
-        values = {'codes': datas}
-        return render(request, 'companyinfo_list.html', values)
-    """
 # Create your views here.
 
 def main_view(request):
